0110fab70e5cd46cc31816723fe0a8a12e145607
acpatison@gmail.com <acpatison@gmail.com>
2021-06-22 16:45:16 -0400

    added models

---
+++ models/workout.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration in minutes"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      // include virtual properties once data is requested
+      virtuals: true
+    }
+  }
+);
+
+// adds dynamic property to schema
+workoutSchema.virtual("totalDuration").get(function() {
+  // reduce array of exercises down to sum of durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
